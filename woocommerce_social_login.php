bbdbe75314ed034f9dd65f0a9a347451506b3b9e
phoeniixx <phoeniixx@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-05 06:52:27 +0000

    readme.txt changes
    git-svn-id: https://plugins.svn.wordpress.org/social-login-lite-for-woocommerce/trunk@1213182 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- woocommerce_social_login.php
+++ woocommerce_social_login.php
@@ -6,7 +6,7 @@
 * Version: 1.0
 * Author: Phoeniixx
 * Author URI: http://www.phoeniixx.com/
-* License: GPL2
+*License: GPLv2 or later*License URI: http://www.gnu.org/licenses/gpl-2.0.html
 */ 
 
 ob_start();
